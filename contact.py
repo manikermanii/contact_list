d0a3652e5b299640471a3e7f7012db64bf1a6400
manikermani <manikermani812@gmail.com>
2026-07-25 11:53:32 +0330

    few update

--- contact.py
+++ contact.py
@@ -1,16 +1,14 @@
 
-class Contact :
+class Contact:
     def __init__(self, name, family, title, number):
         self.name = name
-        self.phone = family
+        self.family = family 
         self.email = title
-        self.address = number 
-
-    def __str__(self):
-        return f"{self.name} {self.family} {self.title} {self.number}"
+        self.address = number
 
     def to_tuple(self):
-        return self.name, self.family, self.title, self.number
+        return (self.name, self.family, self.email, self.address)
+
 
 
 
--- main.py
+++ main.py
@@ -1,36 +1,7 @@
 from contact import Contact
-
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
-
-
-
-contact_list = []
-
-
-def save_click():
-    contact = Contact(name.get(), family.get(), title.get(), number.get())
-    
-
-    contact_list.append(contact)
-
-    table.insert(
-        parent="",
-        index=END,
-        values=tuple(contact.to_tuple())
-    )
-
-    messagebox.showinfo(
-        title="INFO",
-        message="CONTACT SAVED"
-    )
-
-    name.set("")
-    phone.set("")
-    email.set("")
-    address.set("")
-
 
 window = Tk()
 window.title("دفترچه مخاطبین")
@@ -38,86 +9,25 @@ window.geometry("750x350")
 
 
 
-menu = Menu(window)
-window.config(menu=menu)
+contact_list = []
 
-filemenu = Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New")
-filemenu.add_command(label="Open...")
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=window.quit)
+def save_click():
+    name = name.get()
+    family = family.get()
+    title = email.get()
+    number = address.get()
 
-helpmenu = Menu(menu)
-menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About")
+    contact = Contact(name, family, title, number)
+    contact_list.append(contact)
 
+    table.insert(parent="", index=END, values=tuple(contact.to_tuple()))
 
+    messagebox.showinfo(title="INFO", message="CONTACT SAVED")
 
-Label(window, text="نام مخاطب").place(x=20, y=20)
-name = StringVar()
-Entry(window, textvariable=name).place(x=90, y=20)
-
-
-
-Label(window, text="شماره تلفن").place(x=20, y=60)
-phone = StringVar()
-Entry(window, textvariable=phone).place(x=90, y=60)
+    name.set("")
+    family.set("")
+    email.set("")
+    address.set("")
 
 
 
-Label(window, text="ایمیل").place(x=20, y=100)
-email = StringVar()
-Entry(window, textvariable=email).place(x=90, y=100)
-
-
-
-Label(window, text="آدرس").place(x=20, y=140)
-address = StringVar()
-Entry(window, textvariable=address).place(x=90, y=140)
-
-
-
-
-table = ttk.Treeview(
-    window,
-    height=12,
-    columns=[1, 2, 3, 4],
-    show="headings"
-)
-
-
-
-table.column(1, width=120)
-table.column(2, width=120)
-table.column(3, width=150)
-table.column(4, width=120)
-
-
-
-table.heading(1, text="نام مخاطب")
-table.heading(2, text="شماره تلفن")
-table.heading(3, text="ایمیل")
-table.heading(4, text="آدرس")
-
-
-
-table.place(
-    x=230,
-    y=20,
-    width=500,
-    height=250
-)
-
-
-
-
-Button(
-    window,
-    width=10,
-    text="ذخیره",
-    command=save_click
-).place(x=80, y=190)
-
-
-window.mainloop()
